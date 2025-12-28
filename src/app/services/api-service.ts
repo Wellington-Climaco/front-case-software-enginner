17c4eab6953e2993da861250bd4d0a8552ff165b
Wellington <wellington.climaco9@gmail.com>
2025-12-28 15:56:02 -0300

    feat: implementação de consuma da api utilizando a service

--- src/app/services/api-service.ts
+++ src/app/services/api-service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -11,5 +11,17 @@ export class ApiService {
 
   obterTodosRegistros(pagina: number): Observable<obterTodosResponse> {
     return this.http.get<obterTodosResponse>(`${this.baseUrl}/obterTodos/pagina/${pagina}`);
+  }
+
+  removerRegistro(id: string): Observable<Object> {
+    return this.http.delete(`${this.baseUrl}/remover/${id}`);
+  }
+
+  cadastrarPessoa(request: cadastrarPessoaRequest): Observable<pessoaResponse> {
+    return this.http.post<pessoaResponse>(`${this.baseUrl}/cadastrar`, request);
+  }
+
+  atualizarCadastro(request: atualizarCadastroRequest): Observable<pessoaResponse> {
+    return this.http.put<pessoaResponse>(`${this.baseUrl}/atualizar`, request);
   }
 }
